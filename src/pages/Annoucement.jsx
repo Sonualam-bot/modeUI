5e2b9c89b773e3ad1848242c786ac4fbb3afeef9
Sonualam <sonualam1599@gmail.com>
2024-01-05 20:26:38 +0530

    announcement component done

---
+++ src/pages/Annoucement.jsx
@@ -0,0 +1,20 @@
+function Annoucement() {
+  return (
+    <div className=" mx-auto border border-[#E6E7E9] bottom-[1px] ">
+      <div className="w-full flex justify-between sm:justify-center  items-center gap-4 p-[8px] ">
+        <div className="  text-center ">
+          <p className="  p-2 bg-[#E6E7E9] text-[#363C46] rounded-3xl text-sm font-normal ">
+            Announcement
+          </p>
+        </div>
+        <div className="text-xs font-semibold ">
+          <p>
+            We are happy to announce that we raise $2 Million in Seed Funding
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Annoucement;
